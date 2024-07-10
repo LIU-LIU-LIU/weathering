4d45774f3122f0a941e759f10ed31519962ec0d6
liuy-work <liu.liuy@qq.com>
2024-07-10 16:05:27 +0800

    init

---
+++ src/main/java/cc/ahaly/weathering/Weathering.java
@@ -0,0 +1,17 @@
+package cc.ahaly.weathering;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Weathering extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
